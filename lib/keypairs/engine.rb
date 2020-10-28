db3bf682c40d4cc120c96eda3693529bd09b0493
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2020-10-28 07:57:10 +0100

    Make loading of migration explicit

--- lib/keypairs/engine.rb
+++ lib/keypairs/engine.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
 module Keypairs
-  # Rails engine for this gem.
-  # It ensures that the migrations are automatically ran in the applications.
+  # This engine is only needed to add the migration installation rake task.
   class Engine < ::Rails::Engine
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match? "#{root}/"
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
-        # Apartment will modify this, but it doesn't fully support engine migrations,
-        # so we'll reset it here
-        ActiveRecord::Migrator.migrations_paths = app.paths['db/migrate'].to_a
-      end
-    end
+    engine_name 'keypairs'
   end
 end
